1e2b5754e214b41d142ebfbf86ec316151faa956
MillieMD <asdapricesavers@gmail.com>
2024-01-09 00:45:25 +0000

    index layout
    TODO: find banner image
    TODO: fill in blanks activity
    TODO: finish cocktail list
    TODO: css
    TODO: collection editing

---
+++ js/collections.js
@@ -0,0 +1,52 @@
+async function initialiseCollections(){
+
+    let wrapper = document.getElementById("collection-wrapper");
+
+    // Get collections json file
+    let response = await fetch("js/collections.json");
+    response = await response.json();
+    
+    let collections = response["collections"];
+
+    console.log(collections);
+
+    for(i = 0; i < collections.length; i++){
+
+        let currentCollection = collections[i];
+
+        const e = document.createElement("div");
+        e.id = currentCollection["name"];
+        e.classList.add("card");
+        e.id = currentCollection["name"];
+
+        e.insertAdjacentHTML("afterbegin", " <h3> " + currentCollection["name"] + " </h3> <p id = 'status'></p> <div class = 'row'> <button onclick = 'viewCollection("+currentCollection["name"]+")'> View </button> <button onclick = editCollection("+currentCollection["name"]+")> Edit </button> </div>");
+
+        wrapper.append(e);
+
+    }
+
+    let b = document.createElement("button");
+    b.id = "add-collection";
+    b.setAttribute("type", "button");
+    b.setAttribute("onclick","createCollection()");
+    
+    b.innerHTML = "+";
+
+    wrapper.append(b);
+
+}
+
+function viewCollection(collection){
+
+}
+
+function editCollection(collection){
+
+}
+
+function createCollection(){
+
+    
+}
+
+document.onload = initialiseCollections();
---
+++ js/learn.js
@@ -0,0 +1,5 @@
+async function learn(collection){
+
+    
+
+}
